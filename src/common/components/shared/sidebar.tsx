54858d3ec61004458aa55e89f58369897d9bcf12
khalid-walid00 <khalidk50050@gmail.com>
2024-11-29 14:37:11 +0200

    save

--- src/common/components/shared/sidebar.tsx
+++ src/common/components/shared/sidebar.tsx
@@ -115,7 +115,7 @@ function Sidebar() {
             : "md:w-[4%] hidden md:block"
         } transition-all duration-300 h-screen
           right-0
-        overflow-y-auto fixed top-[7.9rem] md:top-0  md:pb-10 pb-20 text-white  `}
+        overflow-y-auto fixed top-[7.9rem] md:top-0  md:pb-10 pb-40 text-white  `}
         style={{
           msOverflowStyle: "none",
           scrollbarWidth: "none",
