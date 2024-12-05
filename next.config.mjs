b29306a262ad267f3fd45035b6b3445ec5631532
khalid-walid00 <khalidk50050@gmail.com>
2024-12-05 15:54:18 +0200

    save

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,7 @@ const nextConfig = {
   experimental: {
     nextScriptWorkers: false,
   }, env: {
-    BASE_URL: process.env.BASE_URL,
-    BASE_CDN: process.env.BASE_CDN,
     MAIN_BASE_URL:process.env.MAIN_BASE_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     MODE:process.env.MODE
   }
 };
