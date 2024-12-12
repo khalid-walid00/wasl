1ded820a1432048513ab8e1e08f7584a3fa3bcbe
khalid-walid00 <khalidk50050@gmail.com>
2024-12-12 04:42:17 +0200

    save

--- src/utils/libraries/axios/axiosClient.ts
+++ src/utils/libraries/axios/axiosClient.ts
@@ -1,53 +1,53 @@
+import axios from "axios";
 import Cookies from "js-cookie";
 import { cookiesValues } from "~/config/constant";
 
-const baseURL = 'http://192.99.33.197:8083/api/v1';
-
-interface FetchParams {
-  [key: string]: any;
-}
-
-type RequestMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
-
-export const fetchDataFromApi = async (
-  endpoint: any,
-  params: any,
-  method: any = 'GET',
-  body?: any
-): Promise<any> => {
-  const url = new URL(`${baseURL}${endpoint}`);
-  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-
-  const token = Cookies.get(cookiesValues.GlobalToken);
-  const headers: HeadersInit = {
+const axiosClient = axios.create({
+  baseURL: process.env.MAIN_BASE_URL, 
+  headers: {
     'Content-Type': 'application/json',
-    Accept: 'application/json, text/plain, */*',
-  };
-  if (token) {
-    headers['Authorization'] = `Bearer ${token}`;
-  }
+    Accept: 'application/json, text/plain, */*'
+  },
+  timeout: 10000, 
+});
 
-  const options: RequestInit = {
-    method,
-    headers,
-    body: body ? JSON.stringify(body) : null,
-  };
-
-  try {
-    const response = await fetch(url.toString(), options);
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.error("API Error:", errorData);
-      throw new Error(`API Error: ${response.statusText}`);
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get(cookiesValues.GlobalToken);
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
-    return await response.json();
-  } catch (error: any) {
-    if (error.name === 'TypeError') {
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosClient.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    if (error.response) {
+      console.error("API Error:", error.response.data);
+    } else if (error.request) {
       console.error("Network Error:", error.message);
     } else {
       console.error("Unexpected Error:", error.message);
     }
-    throw error;
+    return Promise.reject(error);
   }
+);
+
+export const fetchDataFromApi = (
+  endpoint: string, 
+  params?: any, 
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' = "GET", 
+  body?: any
+) => {
+  return axiosClient({
+    url: endpoint,
+    method,
+    params,
+    data: body
+  });
 };
 
+export default axiosClient;
