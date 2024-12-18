d3b34455746d5d6cc4d14efaa18880dfcd105219
khalid-walid00 <khalidk50050@gmail.com>
2024-12-18 14:46:08 +0200

    save

--- src/app/dashboard/companies/components/tableModel/index.tsx
+++ src/app/dashboard/companies/components/tableModel/index.tsx
@@ -15,63 +15,63 @@ function TableModel() {
                         <div key={index} className=" grid grid-cols-2 gap-3">
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Name</CustomLabel>
-                                {item.Name || 'N/A'}
+                                {item?.Name || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>EmailAddress</CustomLabel>
-                                {item.EmailAddress || 'N/A'}
+                                {item?.EmailAddress || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Wasl Id</CustomLabel>
-                                {item.WaslId || 'N/A'}
+                                {item?.WaslId || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Phone Number</CustomLabel>
-                                {item.PhoneNumber || 'N/A'}
+                                {item?.PhoneNumber || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Identity Number</CustomLabel>
-                                {item.IdentityNumber || 'N/A'}
+                                {item?.IdentityNumber || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Created Date</CustomLabel>
-                                {item.CreatedDate || 'N/A'}
+                                {item?.CreatedDate || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Created By</CustomLabel>
-                                {item.CreatedBy || 'N/A'}
+                                {item?.CreatedBy || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>CR Number</CustomLabel>
-                                {item.CommercialRecordNumber || 'N/A'}
+                                {item?.CommercialRecordNumber || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>CRI Date Hijri</CustomLabel>
-                                {item.CommercialRecordIssueDateHijri || 'N/A'}
+                                {item?.CommercialRecordIssueDateHijri || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Date of Birth Gregorian</CustomLabel>
-                                {item.DateOfBirthGregorian || 'N/A'}
+                                {item?.DateOfBirthGregorian || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Extension Number</CustomLabel>
-                                {item.ExtensionNumber || 'N/A'}
+                                {item?.ExtensionNumber || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Manager Name</CustomLabel>
-                                {item.ManagerName || 'N/A'}
+                                {item?.ManagerName || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Manager Phone Number</CustomLabel>
-                                {item.ManagerPhoneNumber || 'N/A'}
+                                {item?.ManagerPhoneNumber || 'N/A'}
                             </div>
                             <div className="flex flex-col gap-1 items-end  text-end">
                                 <CustomLabel bold>Manager Mobile Number</CustomLabel>
-                                {item.ManagerMobileNumber || 'N/A'}
+                                {item?.ManagerMobileNumber || 'N/A'}
                             </div>
                             <div className="flex items-end flex-col gap-2">
                                 <CustomLabel bold>Uplevel Operation Company Id</CustomLabel>
-                                {item.UplevelOperationCompanyId || 'N/A'}
+                                {item?.UplevelOperationCompanyId || 'N/A'}
                             </div>
                         </div>
                     ))
