1b90cf3ce4825228a8e40966becbc1652d568be8
khalid-walid00 <khalidk50050@gmail.com>
2024-12-31 17:25:29 +0200

    Enhance vehicle management table layout with additional columns and improved data display
    - Added new columns for Plate Number, Plate Right Letter, Plate Middle Letter, Plate Left Letter, ImeiNumber, Wasl Vehicle Id, Registration Date, Activity, and Actions to the vehicle management table.
    - Set specific widths for each column to improve layout consistency and readability.
    
    These changes enhance the clarity and usability of the vehicle management interface.

--- src/app/dashboard/vehicle/page.tsx
+++ src/app/dashboard/vehicle/page.tsx
@@ -26,52 +26,62 @@ console.log("itemsSearch", itemsSearch);
         Header: 'Sequence Number',
         accessor: 'SequenceNumber',
         Cell: (tableProps: any) => <p>{tableProps.row.original?.SequenceNumber}</p>,
+        width: '80px',
       },
       {
         Header: 'Plate Number',
         accessor: 'PlateNumber',
         Cell: (tableProps: any) => <p>{tableProps.row.original?.VehiclePlate?.Number}</p>,
+        width: '80px',
       },
       {
         Header: 'Plate Right Letter',
         accessor: 'PlateNumber.RightLetter',
         Cell: (tableProps: any) => <p>{tableProps.row.original?.VehiclePlate?.RightLetter}</p>,
+        width: '70px',
       },
       {
         Header: 'Plate Middle Letter',
         accessor: 'PlateNumber.MiddleLetter',
         Cell: (tableProps: any) => <p>{tableProps.row.original?.VehiclePlate?.MiddleLetter}</p>,
+        width: '70px',
       },
       {
         Header: 'Plate Left Letter',
         accessor: 'PlateNumber.LeftLetter',
         Cell: (tableProps: any) => <p>{tableProps.row.original?.VehiclePlate?.LeftLetter}</p>,
+        width: '70px',
       },
       {
         Header: 'ImeiNumber',
         accessor: 'ImeiNumber',
         Cell: (tableProps: any) => <p>{tableProps.row.original?.ImeiNumber}</p>,
+        width: '90px',
       },
       {
         Header: 'Wasl Vehicle Id',
         accessor: 'WaslId',
         Cell: (tableProps: any) => <p>{tableProps.row.original?.ImeiNumber}</p>,
+        width: '90px',
       },
       {
         Header: 'Registration Date',
         accessor: 'CreatedDate',
         Cell: (tableProps: any) => <p className=" text-nowrap">{tableProps.row.original?.CreatedDate?.replace('T', ' : ').slice(0, 21)}</p>,
+        width: '90px',
       },
       {
         Header: 'Activity',
         accessor: 'Activity',
         Cell: (tableProps: any) => <p>{tableProps.row.original?.Activity}</p>,
+        width: '80px',
       },
       {
         Header: 'Actions',
         Cell: (tableProps: any) => {
           return <ActionList item={tableProps.row.original} />;
         },
+        width: '50px',
       },
     ],
     []
